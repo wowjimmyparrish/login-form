a57d60c9d4295c1502e1e6b54a55e8a39cb10470
James Parrish <jthomasparrishjr@gmail.com>
2023-11-18 15:37:59 -0600

    set up validation in models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    # macro used with bcrypt for authentication/hashing of password
+    has_secure_password 
+
+    # validations for :email and :password
+    validates :email, presence: true, uniqueness: true
+    validates :password, presence: true, length: { in: 6..15 }, confirmation: :true
 end
