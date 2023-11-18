bdf9e9d2bf11a19a5745335c32646326c1d7bf2c
James Parrish <jthomasparrishjr@gmail.com>
2023-11-18 15:24:09 -0600

    Configured client-side routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  
+  # a custom route in our Rails application, to handle any requests that come through that aren't requests for our API routes by returning the public/index.html file instead
+  get '*path',
+      to: 'fallback#index',
+      constraints: ->(req) { !req.xhr? && req.format.html? }
 end
