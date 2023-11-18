91b830e965f16f8437a143bb785e0e871f0c0a37
James Parrish <jthomasparrishjr@gmail.com>
2023-11-18 15:33:07 -0600

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+# custom routes for logging in, signing up, and logging out
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   
   # a custom route in our Rails application, to handle any requests that come through that aren't requests for our API routes by returning the public/index.html file instead
   get '*path',
