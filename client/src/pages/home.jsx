c56dd5b75d43655fe0f8957ea27c04695ccb7b5e
James Parrish <jthomasparrishjr@gmail.com>
2023-11-18 16:20:50 -0600

    configured home page/logout functionality

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { useContext } from 'react'
+import { UserContext } from '../context/user-context'
 
 function Home() {
+  const { user, setUser } = useContext(UserContext);
+
+// This function sends a DELETE request to the /logout endpoint using the fetch function. If the response is successful (status code 200-299), it sets the user to null.
+  function handleLogout() {
+    fetch("/logout", { method: "DELETE" }).then((r) => {
+      if (r.ok) {
+        setUser(null);
+      }
+    });
+  }
+
   return (
-    <div>Home Page</div>
+    <div className='text-center'>
+    <h2>Welcome, {user.email}!</h2>
+    <button onClick={handleLogout}>Logout</button>
+    </div>
   )
 }
 
-export default Home
+export default Home;
