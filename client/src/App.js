849cc037a9037626710db41677e2b25789234e4e
James Parrish <jthomasparrishjr@gmail.com>
2023-11-18 16:06:12 -0600

    added home/login pages, configured user contex, fetched user data

--- client/src/App.js
+++ client/src/App.js
@@ -1,1 +1,37 @@
+import {  useEffect, useContext } from "react";
+import { Route, Switch } from "react-router-dom";
+import { UserContext } from "./context/user-context";
+import Login from "./pages/login";
+import Home from "./pages/home";
 
+function App() {
+  const { user, setUser } = useContext(UserContext);
+ 
+   // fetching user data
+   useEffect(() => {
+    fetch("/me").then((response) => {
+      if (response.ok) {
+        response.json().then((user) => {
+          setUser(user);
+        });
+      }
+    });
+  }, [setUser]);
+
+  if (!user) return <Login  />;
+
+
+  return (
+    <div>
+   <main>
+    <Switch>
+        <Route exact path="/">
+          <Home/>
+        </Route>
+    </Switch>
+    </main>
+  </div>
+  )
+}
+
+export default App;
